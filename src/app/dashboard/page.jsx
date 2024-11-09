ce2698861946d137b61c2cfa982f50587eeecd81
suraj <surajgautam56878@gmail.com>
2024-11-09 19:11:15 +0530

    fix:navigation bar with image

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import dashboard from "../../../public/assets/navbar/dashboard.png";
+import Image from "next/image";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="relative h-screen">
+      {/* Full screen background image */}
+      <Image
+        src={dashboard} // Replace with your image path
+        alt="Background Image"
+        layout="fill"
+        objectFit="cover" // Ensures the image covers the entire div
+        objectPosition="center" // Centers the image within the div
+        priority // Optional: ensures the image is prioritized in loading
+      />
 
-export default Dashboard
+     
+    </div>
+  );
+};
+
+export default Dashboard;
